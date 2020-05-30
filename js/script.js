6690f0bfcf573378ccd7ef2376df70d15c287355
Lukas Vukovich <lukas.vukovich@gmail.com>
2020-05-30 17:32:28 -0400

    Registrater for Activities functionality built

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-// On the page load, focus on the name field
+/* ~~~~ On the page load, focus on the name field ~~~~ */
 
 document.getElementById('name').focus();
 
-// JOB ROLE - Hide other rob role field
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ~~~~ Job Role Functionality: Hide 'Other' field ~~~~ */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 document.getElementById('other-title').hidden = 'true';
 
@@ -14,18 +16,21 @@ document.getElementById('title').addEventListener('input', function (e) {
   }
 });
 
-// // T-Shirt Section
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ~~~~ T-Shirt Functionality: Hide color field if design is not selected ~~~~ */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
 document.getElementById('colors-js-puns').hidden = 'true';
 
 function removeAndAppendDefaultOption() {
   const colorOptions = document.getElementById('color').children;
   for (i = 0; i < colorOptions.length; i++) {
-    if (colorOptions[i].id === 'defaultOption') {
+    if (colorOptions[i].id === 'default-option') {
       colorOptions[i].parentNode.removeChild(colorOptions[i]);
     }
   }
   const defaultOption = document.createElement('OPTION');
-  defaultOption.id = 'defaultOption';
+  defaultOption.id = 'default-option';
   defaultOption.value = 'defaultColor';
   defaultOption.textContent = 'Please select a T-shirt theme';
   defaultOption.selected = true;
@@ -53,4 +58,43 @@ document.getElementById('design').addEventListener('input', function (e) {
   }
 
   removeAndAppendDefaultOption();
+});
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ~~~~ Activities Functionality ~~~~ */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+// Create and Append Total Cost HTML
+const totalCostTag = document.createElement('P');
+totalCostTag.id = 'total-cost';
+totalCostTag.innerText = 'Total: $0';
+const activity = document.querySelector('.activities');
+activity.appendChild(totalCostTag);
+let total = 0;
+
+activity.addEventListener('input', (e) => {
+  let eventDateAndTime = e.target.getAttribute('data-day-and-time');
+  const eventList = activity.querySelectorAll('input');
+
+  for (i = 0; i < eventList.length; i++) {
+    if (
+      eventDateAndTime === eventList[i].getAttribute('data-day-and-time') &&
+      event.target !== eventList[i]
+    ) {
+      if (event.target.checked === true) {
+        eventList[i].disabled = true;
+      } else if (event.target.checked === false) {
+        eventList[i].disabled = false;
+      }
+    }
+  }
+
+  let eventCost = parseInt(e.target.getAttribute('data-cost'));
+
+  if (e.target.checked === true) {
+    total += eventCost;
+  } else {
+    total -= eventCost;
+  }
+  document.getElementById('total-cost').innerText = `Total: $${total}`;
 });
